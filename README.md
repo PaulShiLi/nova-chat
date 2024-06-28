aec226498c35b34520f8f12925f549141c81a9d3
Paul Li <paul.shi.li.05@gmail.com>
2024-06-27 22:28:33 -0700

    Update tests

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Mac:
 ```bash
 rm  ~/.docker/config.json
 ```
+
+To run llama.cpp server after running build.sh:
+```
+./pkg/llama.cpp/llama-server -m ./assets/models/text/mistral-7b/mistral-7b-v0.1.Q2_K.gguf --port 7999 --keep -1 -ngl 0
+```
--- src/test.py
+++ src/test.py
@@ -3,10 +3,7 @@ import requests
 import json
 import time
 
-client = OpenAI(base_url="http://0.0.0.0:8000/v1", api_key="")
-client2 = OpenAI(base_url="http://127.0.0.1:7999/v1", api_key="")
-
-def inference(text: str, endPoint: str = "http://localhost:8000"):
+def inference(text: str, endPoint: str = "http://127.0.0.1:7999"):
     with requests.post(
         f"{endPoint}/completion",
         headers={
@@ -24,68 +21,35 @@ def inference(text: str, endPoint: str = "http://localhost:8000"):
                 content = json.loads(line.decode('utf-8').replace("data: ", ""))["content"]
                 print(content, end="")
 
+def inferenceOpenAI(text: str, endPoint="http://127.0.0.1:7999"):
+    client = OpenAI(base_url=f"{endPoint}/v1", api_key="sk-no-key-required")
+
+    for resp in client.completions.create(
+        model="",
+        prompt=prompt,
+        stream=True,
+        max_tokens=128 # Token limit is bugged
+    ):
+        completion = resp.content
+        print(completion, end="")        
+    
+
 prompt = "Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument."
 
 start = time.time()
 print("====================================")
-# inference(prompt)
-end = time.time()
-
-# print(f"Time taken: {end - start} seconds")
-
-
-start = time.time()
-print("====================================")
-inference(prompt, "http://127.0.0.1:7999")
+inference(prompt)
 end = time.time()
 
 print(f"Time taken: {end - start} seconds")
 
 
+start = time.time()
+print("====================================")
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer, GenerationConfig
+inferenceOpenAI(prompt)
 
-from pathlib import Path
-import os
+end = time.time()
 
-device = "cpu"
-
-root = Path(__file__).resolve().parent.parent
-modelPath = os.path.join(root, "assets", "models", "text", "Storm-7B")
-
-model = AutoModelForCausalLM.from_pretrained(modelPath).to(device)
-tokenizer = AutoTokenizer.from_pretrained(modelPath)
-model.eval().requires_grad_(False)
-
-def generate_response(prompt):
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
-    streamer = TextStreamer(tokenizer=tokenizer, skip_prompt=True, skip_special_tokens=True)
-    
-    generation_params = {
-        "eos_token_id": tokenizer.eos_token_id,
-        "pad_token_id": tokenizer.eos_token_id,
-        "do_sample": True,
-        "temperature": 0.7,
-        "top_p": 0.95,
-        "top_k": 40,
-        "max_new_tokens": 128,
-        "repetition_penalty": 1.1
-    }
-    generation_config = GenerationConfig(**generation_params)
-
-    start = time.time()
-    outputs = model.generate(
-        input_ids, 
-        streamer=streamer,
-        generation_config=generation_config,
-        max_length=128
-    )
-    end = time.time()
-    print(f"Time taken: {end - start} seconds")
-    
-
-prompt = "Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument."
-input_prompt = f"GPT4 Correct User: {prompt}<|end_of_turn|>GPT4 Correct Assistant:"
-# response_text = generate_response(input_prompt)
-# print("Response:", response_text)
+print(f"Time taken: {end - start} seconds")
 
